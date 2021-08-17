338c5778272be89489c97ae2062ec4c29fc4c7c7
enderyasli <enderyasli@gmail.com>
2021-08-17 14:45:27 +0300

    Revert "google login eklendi (fix gerekli)"
    This reverts commit b45877f4

--- app/src/main/java/com/pethiio/android/ui/main/view/login/WelcomeFragment.kt
+++ app/src/main/java/com/pethiio/android/ui/main/view/login/WelcomeFragment.kt
@@ -1,20 +1,11 @@
 package com.pethiio.android.ui.main.view.login
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.pethiio.android.R
 import com.pethiio.android.databinding.FragmentWelcomeBinding
 import com.pethiio.android.ui.base.RegisterBaseFragment
@@ -29,10 +20,6 @@ class WelcomeFragment : RegisterBaseFragment<RegisterBaseViewModel>() {
     override var bottomNavigationViewVisibility = View.GONE
     private var _binding: FragmentWelcomeBinding? = null
     private val binding get() = _binding!!
-    private var signInBtn: SignInButton? = null
-    private var mGoogleSignInClient: GoogleSignInClient? = null
-    val RC_SIGN_IN = 100
-    val TAG: String = WelcomeFragment::class.java.getCanonicalName()
 
 
     override var useSharedViewModel = true
@@ -108,61 +95,9 @@ class WelcomeFragment : RegisterBaseFragment<RegisterBaseViewModel>() {
 
 
         }
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.server_client_id))
-            .requestEmail()
-            .build()
-        mGoogleSignInClient = GoogleSignIn.getClient(activity, gso)
-        signInBtn = view.findViewById(R.id.sign_in_btn)
-        signInBtn!!.setSize(SignInButton.SIZE_STANDARD)
-        signInBtn!!.setOnClickListener { signIn() }
-
 
         return view
 
-    }
-
-    private fun signIn() {
-        val signInIntent: Intent = mGoogleSignInClient!!.signInIntent
-        startActivityForResult(
-            signInIntent,
-            RC_SIGN_IN
-        )
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-        }
-    }
-
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-            updateUI(account)
-        } catch (e: ApiException) {
-            Log.w(
-                TAG,
-                "signInResult:failed code=" + e.statusCode
-            )
-            Toast.makeText(activity, "signInResult:failed code=" + e.statusCode, Toast.LENGTH_SHORT)
-                .show()
-            updateUI(null)
-        }
-    }
-
-    private fun updateUI(account: GoogleSignInAccount?) {
-        if (account == null) {
-            return
-        }
-        val idToken = account.idToken
-
-        Toast.makeText(activity, idToken, Toast.LENGTH_SHORT).show()
     }
 
 
